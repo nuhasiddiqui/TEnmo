1fe669f2b09c2d26e7939cf944c5846430cd3ea9
Nuha Siddiqui <siddiqui.nuha@gmail.com>
2023-08-31 23:50:47 -0500

    completed mapping DAO interfaces for TenmoTransferStatus, TenmoTransferType, and TenmoUser

--- module-2-capstone-team-4/capstone/tenmo-server-THIS WHAT YOU OPEN AND WORK IN/src/main/java/com/techelevator/tenmo/datasource/dao/TenmoTransferTypeDao.java
+++ module-2-capstone-team-4/capstone/tenmo-server-THIS WHAT YOU OPEN AND WORK IN/src/main/java/com/techelevator/tenmo/datasource/dao/TenmoTransferTypeDao.java
@@ -1,4 +1,7 @@
 package com.techelevator.tenmo.datasource.dao;
 
+import com.techelevator.tenmo.datasource.model.TenmoTransferType;
+
 public interface TenmoTransferTypeDao {
+    String getTransferTypeById(int TranferId);
 }
--- module-2-capstone-team-4/capstone/tenmo-server-THIS WHAT YOU OPEN AND WORK IN/src/main/java/com/techelevator/tenmo/datasource/dao/TenmoUserDao.java
+++ module-2-capstone-team-4/capstone/tenmo-server-THIS WHAT YOU OPEN AND WORK IN/src/main/java/com/techelevator/tenmo/datasource/dao/TenmoUserDao.java
@@ -1,4 +1,7 @@
 package com.techelevator.tenmo.datasource.dao;
 
+import com.techelevator.tenmo.datasource.model.TenmoUser
+
 public interface TenmoUserDao {
+    TenmoUser getUsernameById(int usernameId);
 }
