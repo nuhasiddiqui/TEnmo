be6a70e25b9fce457c1190bc0835acda515ca0c9
Nuha Siddiqui <siddiqui.nuha@gmail.com>
2023-09-06 14:38:04 -0500

    update JdbcDaos, Daos, and Models for User, TransferStatus, and TransferType tables

--- module-2-capstone-team-4/capstone/tenmo-server-THIS WHAT YOU OPEN AND WORK IN/src/main/java/com/techelevator/tenmo/datasource/dao/TenmoUserDao.java
+++ module-2-capstone-team-4/capstone/tenmo-server-THIS WHAT YOU OPEN AND WORK IN/src/main/java/com/techelevator/tenmo/datasource/dao/TenmoUserDao.java
@@ -1,6 +1,6 @@
 package com.techelevator.tenmo.datasource.dao;
 
-import com.techelevator.tenmo.datasource.model.TenmoUser
+import com.techelevator.tenmo.datasource.model.TenmoUser;
 
 public interface TenmoUserDao {
     TenmoUser getUsernameById(int usernameId);
